dff3d99b455df0ef081a240d16e338397512dece
Landon-Marcotte <83979177+Landon-Marcotte@users.noreply.github.com>
2021-05-11 13:47:48 -0500

    Add files via upload

---
+++ MainForm.vb
@@ -0,0 +1,22 @@
+﻿Public Class MainForm
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        StartGame()
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ResetAll()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'close the form
+        Me.Close()
+    End Sub
+
+    Private Sub MainForm_Closing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            e.Cancel = False
+        Else
+            e.Cancel = True
+        End If
+    End Sub
+End Class
